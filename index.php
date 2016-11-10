b221bbce24003949e666b3f3d4145a65e12c0848
allan <allanwelerson@gmail.com>
2016-11-10 20:36:44 -0200

    add files end directories

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Musicas</title>
+    
+    <!-- Latest compiled and minified CSS -->
+     <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
+    <link rel="stylesheet" href="css/style.css">
+    
+    
+</head>
+<body>
+    
+       
+            <!-- menu -->        
+                                  
+            <nav class="navbar navbar-default">
+                
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                       
+                          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                         
+                           <a class="navbar-brand" href="#" class="navbar-brand">J-music Select</a>
+                    
+                   
+                    
+                    </div>
+                    
+                    <div class="collapse navbar-collapse" id="myNavbar">
+                    
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">link git</a></li>
+                        <li><a href="#">codepen</a></li>
+                        
+                    </ul>
+                    
+                    </div>
+                    
+                </div>
+                
+                
+            </nav>   
+            
+             
+              <!-- Conteudo --> 
+              
+              <div class="row color-title">
+                  
+                  <div class="col-md-6 col-md-offset-3 text-center ">
+                      <h1>Aqui você esculta suas musicas favoritas!</h1>
+                  </div>
+                  
+              </div>
+              
+              <div class="row">
+                  
+                  <div class="col-md-6 col-md-offset-3">
+                    <form action="">
+                    
+                     <div class="form-inline">
+                         
+                         <div class="form-group">
+                             <label for="Banda">Banda:</label>
+                             <select name="" id="" class="form-control">
+                                 <option value="">Yui</option>
+                                 <option value="">
+                                     One Ok Rock
+                                 </option>
+                                 <option value="">
+                                     Scandal
+                                 </option>
+                                 <option value="">
+                                     Miwa
+                                 </option>
+                             </select>
+                         </div>
+                         <div class="form-group">
+                             <label for="Musicas">Musicas</label>
+                             <select name="" id="" class="form-control">
+                                 <option value="">musica1</option>
+                                 <option value="">musica2</option>
+                                 <option value="">musica3</option>
+                                 <option value="">musica4</option>
+                                 <option value="">musica5</option>
+                                 <option value="">musica6</option>
+                                 <option value="">musica7</option>
+                             </select>
+                             
+                         </div>
+                         
+                     </div>
+                       
+                    </form>
+                      
+                  </div>
+                  
+              </div>
+       
+ 
+    
+    
+    
+    
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+
+<!-- Latest compiled JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>    
+</body>
+</html>
